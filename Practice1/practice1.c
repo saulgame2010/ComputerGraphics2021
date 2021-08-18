fa7d8e640403854643c2ef62832dd60a549aa2e7
Saul Garcia <saulgame2010@gmail.com>
2021-08-17 23:26:29 -0500

    v2
    se cambió la forma de obtener la ecuación de la recta

--- Practice1/practice1.c
+++ Practice1/practice1.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 void main() {
-    int x1, y1, x2, y2;
+    int i;
+    int x1, y1, x2, y2, y;
+    double m, b;
     printf("Enter a pair of numbers (x1, y1) separated by a space\n");
     scanf("%d %d", &x1, &y1);
     printf("Enter a pair of numbers (x2, y2) separated by a space\n");
     scanf("%d %d", &x2, &y2);
-    printf("%d, %d\n", x1, y1);
-    printf("%d, %d\n", x2, y2);
     if((x1 < 0 || y1 > 100)) {
         printf("You must enter numbers between 0 and 100");
         exit(-1);
@@ -16,10 +16,19 @@ void main() {
         if((x2 < 0 || y2 > 100)) {
             printf("You must enter numbers between 0 and 100");
         } else {
-            int i;
-            printf("X\t Y\n");
-            for(i = x1; i < x2; i++){
-                printf("%d\t %d\n", i, (((y2-y1)*i-x1)/(x2-x1))+y1);
+            if(x1 == x2) {
+                printf("The equation is: x=%d", x1);
+            }
+            else if(y1 == y2) {
+                printf("The equation is: x=%d", y1);
+            } else {
+                m = (y2 - y1)/(x2 - x1);
+                b = y1 - (m * x1);
+                printf("X\t Y\n");
+                for(i = x1; i <= x2; i++) {
+                    y = (m * i) + b;
+                    printf("%d\t %d\n", i, y);
+                }
             }
         }
     }
